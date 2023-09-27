ea823783f16e2ab8089e3600a2d4fd4994c2718f
JuHeonHa <hjhjaueon97@naver.com>
2023-09-27 16:40:00 +0900

    initial commit

---
+++ server/index.js
@@ -0,0 +1,25 @@
+const express = require("express");
+const http = require("http");
+const cors = require("cors");
+const { Server } = require("socket.io");
+
+const app = express();
+app.use(cors());
+
+const server = http.createServer(app);
+const io = new Server(server, {
+  cors: {
+    origin: "http://localhost:3000",
+    methods: ["GET", "POST"],
+  },
+});
+
+io.on("connection", (socket) => {
+  console.log(socket.id);
+
+  socket.on("disconnect", (socket) => {
+    console.log("disconnected");
+  });
+});
+
+server.listen(3001, () => console.log("Server is Running"));
